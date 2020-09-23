1fe1ecc4d180b4683f8e663ed3d425296a200969
abid7 <abidshahriar7@gmail.com>
2020-09-23 21:06:00 +0600

    added fontawesome icons

---
+++ js/firefox-fixes.js
@@ -0,0 +1,41 @@
+header.addEventListener("DOMMouseScroll", (e) => {
+  if (e.detail >= 1) {
+    header.classList.add("hide");
+  }
+});
+
+window.addEventListener("DOMMouseScroll", (e) => {
+  if (header.classList.contains("hide")) {
+    upBtn.style.opacity = "1";
+    downBtn.style.opacity = "1";
+  } else {
+    upBtn.style.opacity = "0";
+    downBtn.style.opacity = "0";
+  }
+});
+
+bothFirstSlides.forEach((firstSlide) => {
+  firstSlide.addEventListener("DOMMouseScroll", (e) => {
+    if (e.detail <= 1) {
+      header.classList.remove("hide");
+    }
+  });
+});
+
+bothLastSlides.forEach((slide) => {
+  slide.addEventListener("DOMMouseScroll", (e) => {
+    if (e.detail >= 1 && !footer.classList.contains("visible")) {
+      footer.classList.add("visible");
+      mainSlider.mousewheel.disable();
+      navSlider.mousewheel.disable();
+    }
+    if (e.detail <= 1 && footer.classList.contains("visible")) {
+      footer.classList.remove("visible");
+
+      setTimeout(() => {
+        mainSlider.mousewheel.enable();
+        navSlider.mousewheel.enable();
+      }, 500);
+    }
+  });
+});
